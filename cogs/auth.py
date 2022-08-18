7b3740a4f7a752113c4a5e2d084da1e6e69e1cc2
Влад Булавин <vladon_2004@mail.ru>
2022-08-18 08:08:55 +0300

    added auth timeoute error handler

--- cogs/auth.py
+++ cogs/auth.py
@@ -1,3 +1,4 @@
+from asyncio import futures
 from discord.ext import commands
 from utils.user import UserAuth
 from exceptions import UserAlreadyExists
@@ -15,10 +16,15 @@ class Auth(commands.Cog):
         await interaction.respond(type=6)
 
         await interaction.author.send("Напишите мне в ответ свои логин и пароль от лк")
-        response = await bot.wait_for("message",
-                                      timeout=60,
-                                      check=lambda x: x.author.id == interaction.author.id
-                                      )
+        try:
+            response = await bot.wait_for("message",
+                                          timeout=60,
+                                          check=lambda x: x.author.id == interaction.author.id
+                                          )
+        except futures.TimeoutError:
+            await interaction.author.send("Время ожидания вышло. Повторите все шаги заново")
+            return
+
         try:
             login, password = response.content.split()
         except ValueError:
--- utils/user/UserAuth.py
+++ utils/user/UserAuth.py
@@ -14,12 +14,8 @@ class UserAuth:
             await member.send("Неверный логин или пароль")
             return
 
-        except ServerNotResponds:
+        except (ServerNotResponds, ServerError):
             await member.send("Авторизация в данный момент не доступна. Попробуйте позже")
-            return
-
-        except ServerError:
-            await member.send("Авторизация в данный момент не доступна. Попробуйте позже. ServerError")
             return
 
         if user_info['department'] != "5dd48625-77d5-11e9-940d-000c29c02919":
