50acffa0667137160d5bd16f948ec748bb189f81
Влад Булавин <vladon_2004@mail.ru>
2022-08-19 11:55:47 +0300

    wrapped changing username in try/catch block

--- utils/user/UserAuth.py
+++ utils/user/UserAuth.py
@@ -34,7 +34,10 @@ class UserAuth:
         await UserManager.addRoles(member, role, reason="Added by bot, adding new user role")
         await UserManager.addRoles(member, studentRole, reason="Added by bot, adding new user role")
 
-        await UserManager.changeNickname(member, nick=nickname, reason="Changed by bot, authenticating new user")
+        try:
+            await UserManager.changeNickname(member, nick=nickname, reason="Changed by bot, authenticating new user")
+        except Exception:
+            pass
 
         await member.send(f"Вы успешно авторизованы, {user_info['name']}!")
         return user_info
